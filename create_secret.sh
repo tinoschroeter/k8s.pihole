bbab1f32e5f1cd7f4f2f1451f76726a2bb4371cc
Tino Schröter <tino.schroeter@eventim.de>
2026-04-03 16:05:22 +0200

    update to newer version

--- create_secret.sh
+++ create_secret.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -z "$1" ]; then
+if [ -n "$1" ]; then
   cat <<EOF | kubectl apply -f -
   apiVersion: v1
   kind: Secret
@@ -8,9 +8,9 @@ if [ ! -z "$1" ]; then
     name: pihole-secret
   type: Opaque
   data:
-    password: $(echo -n $1 | base64 -w0)
+    password: $(echo -n "$1" | base64 -w0)
 EOF
 
-else 
+else
   echo "Password missing..."
 fi
